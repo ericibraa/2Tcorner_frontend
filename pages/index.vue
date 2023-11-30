94031c1b451f72d09ce5546b0cf9ff4b8eb49d08
Afrizal Eric Ibrahim <erikibrahim76@gmail.com>
2023-12-01 00:05:10 +0700

    pull n edit laptop rumah

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <v-carousel
-      v-model="model"
-      cycle
-      hide-delimiter-background
-      continuous
-      hide-delimiters
-      show-arrows-on-hover
-    >
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">Slide {{ i + 1 }}</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
+    <div class="carousel pb-5">
+      <v-carousel
+        cycle
+        hide-delimiter-background
+        continuous
+        hide-delimiters
+        show-arrows-on-hover
+        width="100%"
+        height="100%"
+      >
+        <v-carousel-item>
+          <v-img
+            src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2023/11/29/a8632885-5592-4948-8dbb-83a7bb45157c.jpg.webp?ect=4g"
+          ></v-img>
+        </v-carousel-item>
+      </v-carousel>
+    </div>
     <v-row>
       <v-col cols="12" lg="8">
         <v-card elevation="5">
@@ -25,7 +26,8 @@
           <v-card-text>
             <v-btn max-height="100">
               <v-img
-                :src="require('~/assets/images/brandLogo/yamaha.png')" max-width="50"
+                :src="require('~/assets/images/brandLogo/yamaha.png')"
+                max-width="50"
               ></v-img>
             </v-btn>
           </v-card-text>
@@ -45,9 +47,13 @@ export default {
   name: "IndexPage",
   data() {
     return {
-      model: 0,
-      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
     };
   },
 };
 </script>
+
+<style scoped>
+.carousel {
+  border-radius: 10px;
+}
+</style>
